735a0246fef6e3ba0a8bcc0e9c560446fd95e800
Julio Cezar Froes <jsfroes336@gmail.com>
2021-03-16 18:45:44 +0000

    create dummy data in the DB. Link DB to the home view page

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'jsfroes',
-        'title': 'blog post 1',
-        'content': 'first post content',
-        'date_posted': 'March 16, 2021'
-    },
-      {
-        'author': 'charlotte',
-        'title': 'blog post 2',
-        'content': 'second post content',
-        'date_posted': 'March 18, 2021'
-    },
-]
-
-
+from .models import Post
 
 def home(req):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
 
     return render(req, 'blog/home.html', context)
