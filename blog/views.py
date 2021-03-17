4f9d05555130cc07da380bfc9de977c69051a90a
Julio Cezar Froes <jsfroes336@gmail.com>
2021-03-17 10:04:03 +0000

    create a class based view to the home page

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
+# function based views 
 def home(req):
     context = {
         'posts': Post.objects.all()
@@ -15,3 +16,13 @@ def about(req):
     }
     
     return render(req, 'blog/about.html', title)
+
+# class based views
+# there are different kinds of class based views - List views, update views, delete views, detail views
+
+from django.views.generic import ListView
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
