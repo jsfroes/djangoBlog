349614cdc72dca094c714cfbecdcb264b2edcc9c
Julio Cezar Froes <jsfroes336@gmail.com>
2021-03-17 10:29:07 +0000

    add create new post using class based views

--- blog/views.py
+++ blog/views.py
@@ -20,9 +20,22 @@ def about(req):
 # class based views
 # there are different kinds of class based views - List views, update views, delete views, detail views
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html - standard path
     context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # set the author for the blog post
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
