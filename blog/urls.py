9a32b208122599031f17a6e7e9e569111fe5dc42
Julio Cezar Froes <jsfroes336@gmail.com>
2021-03-17 10:48:04 +0000

    add listview, create, update and delete post

--- blog/urls.py
+++ blog/urls.py
@@ -2,13 +2,17 @@ from django.urls import path
 from .views import (
     PostListView, 
     PostDetailView, 
-    PostCreateView
+    PostCreateView, 
+    PostUpdateView,
+    PostDeleteView
 )
 from . import views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='blog-home'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('', PostListView.as_view(), name='blog-home'), #home.html
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), #post_detail.html
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'), #post_form.html
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), #post_confirm_delete.html
+    path('post/new/', PostCreateView.as_view(), name='post-create'), #post_form.html
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-# function based views 
+# function based views
+
+
 def home(req):
     context = {
         'posts': Post.objects.all()
@@ -10,28 +14,30 @@ def home(req):
 
     return render(req, 'blog/home.html', context)
 
+
 def about(req):
     title = {
         'title': 'About'
     }
-    
+
     return render(req, 'blog/about.html', title)
 
 # class based views
 # there are different kinds of class based views - List views, update views, delete views, detail views
 
-from django.views.generic import ListView, DetailView, CreateView
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html - standard path
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html - standard path
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
 
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(CreateView):
+
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
@@ -39,3 +45,29 @@ class PostCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # set the author for the blog post
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
